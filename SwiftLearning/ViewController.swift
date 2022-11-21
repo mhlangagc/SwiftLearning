9076e34755a62a6f0762026c0d8179594867bbeb
Gugulethu <mhlangagc@gmail.com>
2022-11-21 17:19:12 +0200

    Initial Commit

---
+++ SwiftLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLearning
+//
+//  Created by Gugulethu Mhlanga on 2022/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
