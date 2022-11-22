9fa364bd09d52040a643b02897c517bcd4914ae1
Gugulethu <mhlangagc@gmail.com>
2022-11-23 01:42:13 +0200

    updated the ui to accomodate a different approach

--- SwiftLearning/HomeViewController.swift
+++ SwiftLearning/HomeViewController.swift
@@ -2,32 +2,35 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    lazy var tapMeButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Tap Me", for: UIControl.State.normal)
-        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
-        button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(handleTapButton), for: UIControl.Event.touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
-
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
-        setupButtonView()
+        view.backgroundColor = UIColor.blue
+        setupButtonUI()
     }
     
-    func setupButtonView() {
-        view.addSubview(tapMeButton)
-        tapMeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        tapMeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tapMeButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        tapMeButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
+    func setupButtonUI() {
+        // Changing the Attributes of the button
+        button.setTitle("Tap Me", for: .normal)
+        button.setTitleColor(UIColor.white, for: .normal)
+        button.backgroundColor = .red
+        button.layer.cornerRadius = 20
+        button.addTarget(self, action: #selector(handleTapButton), for: UIControl.Event.touchUpInside)
+        
+        view.addSubview(button)
+        
+//        button. frame = CGRect(x: 100, y: 400, width: 200, height: 50)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 150).isActive = true
     }
     
     @objc func handleTapButton() {
-        tapMeButton.setTitle("Don't Tap Me", for: UIControl.State.normal)
+        button.setTitle("Don't Tap Me", for: UIControl.State.normal)
+
     }
 
 }
