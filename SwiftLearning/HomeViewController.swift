03dddc6f18aa92f9dbbd46fc09ad72d771def68b
Gugulethu <mhlangagc@gmail.com>
2022-11-23 08:34:54 +0200

    adding a button and label with constraints

--- SwiftLearning/HomeViewController.swift
+++ SwiftLearning/HomeViewController.swift
@@ -2,32 +2,48 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
-    lazy var tapMeButton: UIButton = {
+    let aButton : UIButton = {
         let button = UIButton()
-        button.setTitle("Tap Me", for: UIControl.State.normal)
-        button.setTitleColor(UIColor.white, for: UIControl.State.normal)
-        button.backgroundColor = UIColor.red
-        button.addTarget(self, action: #selector(handleTapButton), for: UIControl.Event.touchUpInside)
+
+        button.setTitle("Tap Me", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
+        button.frame = CGRect(x: 100, y: 400, width: 200, height: 50)
+        button.layer.cornerRadius = 25
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self , action: #selector(showInLabel), for: .touchUpInside)
         return button
+    }()
+        
+    @objc func showInLabel() {
+        aLabel.text = "Coding is Fun."
+    }
+    
+    let aLabel : UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.backgroundColor = .systemGreen
+        label.frame = CGRect(x: 100, y: 400, width: 200, height: 50)
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor.white
-        setupButtonView()
+        
+        view.addSubview(aButton)
+        view.addSubview(aLabel)
+        
+        aButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        aButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        aButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        aButton.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        aButton.topAnchor.constraint(equalTo: aLabel.bottomAnchor, constant: 10).isActive = true
+        
+        aLabel.centerXAnchor.constraint(equalTo: aButton.centerXAnchor).isActive = true
+        aLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        aLabel.widthAnchor.constraint(equalTo: aButton.widthAnchor).isActive = true
     }
-    
-    func setupButtonView() {
-        view.addSubview(tapMeButton)
-        tapMeButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        tapMeButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        tapMeButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        tapMeButton.widthAnchor.constraint(equalToConstant: 150).isActive = true
-    }
-    
-    @objc func handleTapButton() {
-        tapMeButton.setTitle("Don't Tap Me", for: UIControl.State.normal)
-    }
-
 }
